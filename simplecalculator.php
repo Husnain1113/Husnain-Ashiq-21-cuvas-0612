2e8d977c5f5884d51dca10a9113c4fd4faa98da4
Husnain1113 <146130301+Husnain1113@users.noreply.github.com>
2023-09-27 22:59:11 -0700

    Add files via upload

---
+++ simplecalculator.php
@@ -0,0 +1,17 @@
+<?php
+$a=60;
+$b=10; 
+$sum=  $a + $b  ;
+$sub=  $a - $b  ;
+$multiple=  $a * $b  ;
+$divison=  $a / $b  ;
+echo "<b>Addition=</b>".$sum;
+echo "<br>";
+echo "<b>Subtraction=</b>".$sub;
+echo "<br>";
+echo "<b>multiply=</b>".$multiple;
+echo "<br>";
+echo "<b>division=</b>".$divison;
+echo "<br>";
+
+?>
